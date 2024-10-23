82ccdda716f95c76a3d04a29fcd11d6533edb0e9
Arabinda Mohanty <35189540+arabinda2018@users.noreply.github.com>
2024-10-23 15:20:53 +0530

    Remove unwanted space
    According coding standard we shouldn't add any extra line .

--- src/pac10/B.java
+++ src/pac10/B.java
@@ -5,8 +5,7 @@ public class B {
 		System.out.println("from iib");
 	}
 	public static void main(String[] args) {
-		B b1=new B();
-		
+		B b1=new B();//remove unwanted space
 		B b2 =new B();
 	}
 }
